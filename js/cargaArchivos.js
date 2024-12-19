3e7b8688ff82091b2ef6a5a09cea212ce46b85a9
Ruben <ruben.serrano@iesdoctorbalmis.com>
2024-12-19 12:15:49 +0100

    Añadir encabezado y barra de navegación responsiva con integración de Bootstrap

---
+++ js/cargaArchivos.js
@@ -0,0 +1,17 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        // Crear un contenedor temporal para manipular el HTML
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = data;
+
+        // Extraer solo el contenido con el id "header"
+        const headerFragment = tempDiv.querySelector('#header');
+        document.getElementById('header').innerHTML = headerFragment ? headerFragment.outerHTML : '';
+
+    });
+
+
+/*fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data);*/
